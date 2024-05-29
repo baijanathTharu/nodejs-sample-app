14b350b0b66640088bfd2aab2e1efd17d59f9287
baijanathTharu <bnchaudhary.tharu@gmail.com>
2024-05-29 22:23:35 +0545

    use dotenv

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 80;
 
